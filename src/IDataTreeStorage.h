690c314ba19c1ce30938e577727c52e5005c7db3
Krutix <pigeon.krutix@gmail.com>
2020-10-25 11:28:12 -0700

    [remake] Player widget (simple)

--- src/IDataTreeStorage.h
+++ src/IDataTreeStorage.h
@@ -1,13 +1,16 @@
 #ifndef IDATATREESTORAGE_H
 #define IDATATREESTORAGE_H
 
-#include "datatree.h"
+#include "DataTree.h"
 
 template<class T>
 class IDataTreeStorage
 {
+protected:
+    QString nodeName;
+public:
     virtual DataTree<T> getData() = 0;
-    virtual void setData(DataTree<T> data) = 0;
+    virtual void setData(const DataTree<T>& data) = 0;
     virtual void resetData() = 0;
 };
 
--- src/Src.pri
+++ src/Src.pri
@@ -1,6 +1,11 @@
 SOURCES += \
+    $$PWD/PlayerWidget.cpp \
     $$PWD/main.cpp
 
 HEADERS += \
     $$PWD/DataTree.h \
-    $$PWD/IDataTreeStorage.h
+    $$PWD/IDataTreeStorage.h \
+    $$PWD/PlayerWidget.h
+
+FORMS += \
+    $$PWD/PlayerWidget.ui
